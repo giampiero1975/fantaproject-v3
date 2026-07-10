5fa0824563319c92bd1bffe5da76f757d63f44b6
giampiero1975 <giampiero.digregorio@gmail.com>
2026-07-10 19:41:30 +0200

    align account dropdown with dark application theme

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,9 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white', 'dropdownClasses' => ''])
+@props([
+    'align' => 'right',
+    'width' => '48',
+    'contentClasses' => 'py-1 bg-[#0B1730] border border-white/10 text-slate-200',
+    'dropdownClasses' => '',
+])
 
 @php
 $alignmentClasses = match ($align) {
@@ -20,17 +25,19 @@ $width = match ($width) {
         {{ $trigger }}
     </div>
 
-    <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+    <div
+        x-cloak
+        x-show="open"
+        x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="transform opacity-0 scale-95"
+        x-transition:enter-end="transform opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-75"
+        x-transition:leave-start="transform opacity-100 scale-100"
+        x-transition:leave-end="transform opacity-0 scale-95"
+        class="absolute z-50 mt-2 {{ $width }} rounded-xl shadow-2xl shadow-black/35 {{ $alignmentClasses }} {{ $dropdownClasses }}"
+        @click="open = false"
+    >
+        <div class="overflow-hidden rounded-xl ring-1 ring-white/10 {{ $contentClasses }}">
             {{ $content }}
         </div>
     </div>
