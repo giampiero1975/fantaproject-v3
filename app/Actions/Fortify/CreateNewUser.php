f3c5bbf44193069dba24533a6804595591545d39
giampiero1975 <giampiero.digregorio@gmail.com>
2026-07-16 11:35:06 +0200

    Fix registration when default role is missing

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Spatie\Permission\Models\Role;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -32,7 +33,8 @@ class CreateNewUser implements CreatesNewUsers
             'password' => Hash::make($input['password']),
         ]);
 
-        $user->assignRole('user');
+        $defaultRole = Role::findOrCreate('user', config('auth.defaults.guard', 'web'));
+        $user->assignRole($defaultRole);
 
         return $user;
     }
